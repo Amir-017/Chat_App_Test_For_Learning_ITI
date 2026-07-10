680ccb1f58554ea6c72a209cb8d736e606bbd6c7
amir whdan <amiirahmed017@gmail.com>
2026-07-10 12:29:28 +0300

    handle page messages.controller.js and next step is save messages in page index.js

---
+++ server/src/controller/messages.controller.js
@@ -0,0 +1,42 @@
+const Message = require("../models/message.model");
+////////////////////////////////
+
+ // Get All Messages
+
+//////////////////////////////////
+const allMessages = async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//////////////////////////////////
+
+ // Edit Message
+
+//////////////////////////////////
+const editeMessage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { message } = req.body;
+
+    const updatedMessage = await Message.findByIdAndUpdate(
+      id,
+      { message },
+      { new: true }
+    );
+
+    if (!updatedMessage) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+
+    res.status(200).json(updatedMessage);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.allMessages = allMessages;
