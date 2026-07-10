77405a5c78c7990dfef1213f86d8dcdcd853b3d2
amir whdan <amiirahmed017@gmail.com>
2026-07-10 12:19:12 +0300

    handle connect with server and mongoose and finish part of web socket

---
+++ server/index.js
@@ -0,0 +1,54 @@
+// require modules
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// all the middlewares
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+
+const server = http.createServer(app);
+
+// socket.io
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+    },
+});
+
+io.on("connection", (socket) => {
+
+    console.log("User Connected:", socket.id);
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+    });
+    socket.on("send-message", async ({ message, receiver, sender }) => {
+
+
+        io.to(receiver).emit("receive-message", { message, sender, receiver });
+
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected");
+    });
+
+});
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/chat-app").then(() => {
+    console.log("MongoDB Connected");
+})
+
+// start the server
+server.listen(3000, () => {
+    console.log("Server Running at Port 3000");
+});
+
