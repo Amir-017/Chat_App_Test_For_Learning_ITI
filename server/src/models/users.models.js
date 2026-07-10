1141e66c32e1b0eb0bec13d17c9e272f9d38e4ce
amir whdan <amiirahmed017@gmail.com>
2026-07-10 12:24:32 +0300

    handle page users.models.js and next step is users.controller

---
+++ server/src/models/users.models.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+
+    lastSeen: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+ 
+});
+module.exports = mongoose.model("User", userSchema);
